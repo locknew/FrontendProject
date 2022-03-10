9265c7561c48dc7d09422a44971b991d706c0fef
Suradit Luo <suradit_luo@cmu.ac.th>
2022-03-10 10:52:41 +0700

    recover data loss during previous commit

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const ejs = require("ejs");
 var _ = require('lodash');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.static(__dirname + './public/script.js'));
 
 app.get("/", function(req,res){
-    res.render('list.ejs', {
-    }); 
+    res.render('list.ejs'); 
 })
-
 app.listen('3000', function(){
     console.log("server start at port 3000")
 })
