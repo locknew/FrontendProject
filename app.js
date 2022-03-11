a7db05fb2710e0041e4f41ec720609afc2cd8be6
thictikorne vin <bangbang12353@gmail.com>
2022-03-12 03:16:46 +0700

    commit kitchen login

--- app.js
+++ app.js
@@ -3,21 +3,62 @@ const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
 var _ = require('lodash');
-
+const { MongoClient } = require("mongodb");
+const console = require("console");
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(express.static("public"));
 app.use(express.static(__dirname + './public/script.js'));
+const client = new MongoClient('mongodb+srv://locknew:12345@cluster0.2uz6u.mongodb.net/Restaurant?retryWrites=true&w=majority');
+var loggedIn = [];             
 
-
-app.get("/", function(req,res){
-    res.render('list.ejs'); 
+app.get("/", function(req, res) {
+    res.render('list.ejs');
 })
 
-app.get("/kitchen", function(req,res){
-    res.render('kitchen.ejs'); 
+app.get("/kitchen", function(req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    res.render('kitchen.ejs', { isLoggedIn: req.query.l === "true" || loggedIn.includes(ip) });
 })
 
-app.listen('3000', function(){
+app.post("/login", async function(req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    await client.connect()
+    const db = client.db('Restaurant').collection('Chef');;
+    var resBody = {
+        status: "success",
+        data: {}
+    };
+    try {
+        const user = req.body["value"]
+        if (user["Username"].length > 0 && user["Password"].length > 0) {
+            const data = await db.findOne({ "Username": user["Username"] });
+            if (data !== null) {
+                if (data["Password"] === user["Password"]) {
+                    loggedIn.push(ip);
+                    console.log(`${ip} logged in.`)
+                } else resBody["status"] = "error";
+            } else resBody["status"] = "error";
+        } else resBody["status"] = "error";
+    } catch (err) {
+        resBody["status"] = "error";
+    }
+    res.json(resBody);
+})
+
+app.post("/logout", async function(req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    var resBody = {
+        status: "success",
+        data: {}
+    };
+    loggedIn.splice(loggedIn.indexOf(ip), 1)
+    console.log(`${ip} logged out.`)
+    res.json(resBody);
+})
+
+
+app.listen('3000', function() {
     console.log("server start at port 3000")
 })
--- public/script.js
+++ public/script.js
@@ -1,12 +1,8 @@
-document.getElementById('submit').disabled = true;
-var foodList = [];
-var quanti = document.getElementById("foodQt").children[1].innerText;
-var Fname, request, qt;
-var orderID = 0;
-
 var foodlist = [];
 var quanti = document.getElementById("foodQt").children[1].innerText;
 var Fname, request, qt;
+
+
 
 $(".mod").click(function(){
     $('.modal').modal('show');
@@ -38,21 +34,6 @@ function substract(){
 function closeMod(){
     $('.modal').modal('hide');
 }
-async function submit(){
-    let tableNum = document.getElementById('sel-number').value;
-    if(document.getElementById("foodQt").children[1].innerText != 0){
-        orderID++;
-        Fname = document.getElementById("modLabel").innerText;
-        request = document.getElementById("message-text").value;
-        qt = document.getElementById("foodQt").children[1].innerText;
-        foodList.push(['order'+orderID,tableNum, Fname, request, qt])
-        addOrder(orderID, Fname, request, qt);
-        document.getElementById('submit').disabled = false;
-    }
-    for(let i = 0; i < foodList.length; i++){
-        console.log(foodList[i]);
-    }
-
 function submit(){
     if(document.getElementById("foodQt").children[1].innerText != 0){
        Fname = document.getElementById("modLabel").innerText;
@@ -84,63 +65,7 @@ function searchFood(){
     }
 }
 
-function addOrder(Id, name, request, qt){
-    
-    let orderBox = document.createElement("div");orderBox.className="orderList";
-    let orderRow = document.createElement("div");orderRow.className="row";    
-    let orderNameBox = document.createElement("div");orderNameBox.className="foodOr col-10 col-sm-10";
-    let orderName = document.createElement("h2");orderName.innerHTML=name;
-    let deleteButtonBox = document.createElement("div");deleteButtonBox.className="deleteOrder col-2 col-sm-2";
-    let deleteButton = document.createElement("h2");deleteButton.setAttribute('id','order'+Id);deleteButton.setAttribute('role','button');deleteButton.setAttribute('onclick','deleteOrder(this.id)');deleteButton.innerHTML = '-';
-    let requestRow = document.createElement("div");requestRow.className="row";
-    let requestBox = document.createElement("div");requestBox.className="request col-10 col-sm-10";
-    let requestMessage = document.createElement("h5");requestMessage.innerHTML=request;
-    let qtBox = document.createElement("div");qtBox.className="qt col-2 col-sm-2";
-    let qtTxt = document.createElement("h3");qtTxt.innerHTML=qt;qtTxt.className="qt";
-    
-    orderNameBox.appendChild(orderName);deleteButtonBox.appendChild(deleteButton);
-    orderRow.appendChild(orderNameBox);orderRow.appendChild(deleteButtonBox);
-    orderBox.appendChild(orderRow);
-    requestBox.appendChild(requestMessage);qtBox.appendChild(qtTxt);
-    requestRow.appendChild(requestBox);requestRow.appendChild(qtBox);
-    orderBox.appendChild(requestRow);
-
-    document.getElementById("orderRow").prepend(orderBox);
-}
-
-function deleteOrder(orderID){
-    console.log(orderID)
-    if(confirm('Do you want to remove this order?') == true){
-        document.getElementById(orderID).parentNode.parentNode.parentNode.remove();
-        for (let i = 0; i < foodList.length; i++) {
-            if(foodList[i][0] == orderID){
-                foodList.splice(i,1)
-            }
-        } 
-    }
-    checkSubmit();
-}
-
-function checkSubmit(){
-    if(foodList.length < 1){
-        document.getElementById('submit').disabled = true;
-    } else {
-        document.getElementById('submit').disabled = false;
-    }
-}
-}
 /* Kitchen js*/ 
-var Chef = [
-  
-    {
-      Username: "Thictikorne",
-      Password: "vin"
-    },
-    {
-      Username: "Suradit",
-      Password: "Luo"
-    }
-  ]
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
@@ -148,23 +73,12 @@ var Chef = [
   }
   
   
-  
+
 
 function ChefLogin(){
 
     var username = document.querySelector('#Username').value
-    var password = document.querySelector("Password").value
-   
-    for(i = 0;i<Chef.length;i++){
-        if(username ==Chef[i].Username&&password==Chef[i].Password)
-        {
-            alert("TRUE")
-            return
-        }
+    var password = document.querySelector("#Password").value
     
-        }
-        alert ("FALSE")
-    }
-
-    
-    
+ 
+  }
